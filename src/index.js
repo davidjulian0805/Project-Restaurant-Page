dd8f680a8da467f1f8dc086347b5d607e6b0a8cd
David Julian <davidjoshuajulian8@gmail.com>
2026-03-19 12:08:08 +0800

    added setActive function for logo to be redirected to home page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import "./styles.css";
+import logoImg from "./Images/logo.png";
 import loadHomePage from "./home.js";
 import loadMenuPage from "./menu.js";
 import loadContactPage from "./contact.js";
 
-
+document.getElementById("logo-img").src = logoImg;
 
 const clearContent = () => {
   const content = document.getElementById("content");
@@ -11,20 +12,36 @@ const clearContent = () => {
 };
 
 
-loadHomePage();
+const setActive = (id) => {
+  document.querySelectorAll("nav button").forEach((btn) => {
+    btn.classList.remove("active");
+  });
+  document.getElementById(id).classList.add("active");
+};
 
+loadHomePage();
+setActive("btn-home");
+
+document.getElementById("logo-img").addEventListener("click", () => {
+  clearContent();
+  loadHomePage();
+  setActive("btn-home");
+});
 
 document.getElementById("btn-home").addEventListener("click", () => {
   clearContent();
   loadHomePage();
+  setActive("btn-home");
 });
 
 document.getElementById("btn-menu").addEventListener("click", () => {
   clearContent();
   loadMenuPage();
+  setActive("btn-menu");
 });
 
 document.getElementById("btn-contact").addEventListener("click", () => {
   clearContent();
   loadContactPage();
+  setActive("btn-contact");
 });
